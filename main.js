8eb3af42fe9ba6f75ce8515adb36ebba1f548453
Ahrorbek7148 <ahrorbek.tursunboev.1999@outlook.com>
2022-04-25 13:51:36 +0500

    components changed

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#user__social").click(function(){
+        $("#social__modal").html("<div style = 'background-color: #a8a8a8, padding: 2rem;' class='d-flex justufy-content-centre '>" + "<a class='text-tecoretion-none' href='http://instagram.com'><i class='fa fa-instagram'></i></a>" + "<a class='text-tecoretion-none' href='http://facebook.com'><i class='fa fa-facebook'></i></a>" +"</div>");
+    })
+})
